d4afbfbea40b581388031ad0c25b1a5d5a4b7bb3
ojaswinjosan <ojaswin.josan@gmail.com>
2020-06-18 11:18:43 +1000

    Add an HTML Page

--- container-files/main.py
+++ container-files/main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Homepage
 @app.route("/")
 def index():
-    return "Text"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
